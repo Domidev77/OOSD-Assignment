42adc5a675cf5345431956f8888f6e158314498c
Albert Bielecki <albert.bielecki@sonovate.com>
2022-04-06 01:19:31 +0100

    Added some more rooms

--- src/main/java/Assignment/Data/AccommodationData.java
+++ src/main/java/Assignment/Data/AccommodationData.java
@@ -16,8 +16,10 @@ public class AccommodationData {
 
     List<Room> studentVillageRooms = Arrays.asList(
             new Room(101, 500.0, "Clean", "Single bedroom with en-suite", new Lease(1, 12, new Student("John Smith", 20010010))),
-            new Room(102, 850.0, "Offline", "Single bedroom with en-suite", null),
-            new Room(103, 550.0, "Clean", "Single bedroom with en-suite, nice view", new Lease(3, 12, new Student("Victoria Baker", 20010011)))
+            new Room(102, 850.0, "Offline", "Single bedroom with en-suite, nice view", null),
+            new Room(103, 550.0, "Clean", "Single bedroom with en-suite", new Lease(2, 12, new Student("Victoria Baker", 20010011))),
+            new Room(104, 650.0, "Dirty", "Single bedroom", new Lease(3, 12, new Student("Sarah Kowalska", 20010017))),
+            new Room(105, 600.0, "Clean", "Single bedroom", new Lease(4, 12, new Student("Andy Knight", 20010019)))
     );
     //-------------------------------------------------------
 
@@ -26,10 +28,11 @@ public class AccommodationData {
     HallWarden hallWarden2 = new HallWarden();
 
     List<Room> wallscourtParkRooms = Arrays.asList(
-            new Room(201, 500.0, "Clean", "Single bedroom with en-suite", new Lease(1, 12, new Student("Jason Holland", 20010012))),
-            new Room(202, 850.0, "Clean", "Single bedroom with en-suite", new Lease(2, 12, new Student("Anne Doe", 20010013))),
+            new Room(201, 500.0, "Clean", "Single bedroom with en-suite", new Lease(5, 12, new Student("Jason Holland", 20010012))),
+            new Room(202, 850.0, "Clean", "Single bedroom with en-suite", new Lease(6, 12, new Student("Anne Doe", 20010013))),
             new Room(203, 550.0, "Dirty", "Single bedroom with en-suite, nice view", null),
-            new Room(204,550.0,"Clean","Single bedroom with en-suite, nice view",null)
+            new Room(204,550.0,"Clean","Single bedroom with en-suite, nice view",null),
+            new Room(205, 600.0, "Dirty", "Single bedroom", new Lease(7, 12, new Student("Bob Smith", 20030010)))
     );
     //-------------------------------------------------------
 
